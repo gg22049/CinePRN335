5c2d7ab5b8f419e25ce3a5993dc41da120a8fdc9
gg22049 <gg22049@ues.edu.sv>
2024-11-13 23:51:13 -0600

    Calendario sin funciones agregado

---
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/boundary/jsf/FrmProgramacion.java
@@ -0,0 +1,80 @@
+package sv.edu.ues.occ.ingenieria.prn335_2024.cine.boundary.jsf;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import org.primefaces.model.DefaultScheduleEvent;
+import org.primefaces.model.LazyScheduleModel;
+import sv.edu.ues.occ.ingenieria.prn335_2024.cine.control.ProgramacionBean;
+import sv.edu.ues.occ.ingenieria.prn335_2024.cine.entity.Programacion;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Named
+@ViewScoped
+public class FrmProgramacion implements Serializable {
+
+    @Inject
+    ProgramacionBean bean;
+
+    @Inject
+    FacesContext fc;
+
+    protected Long idSala;
+    protected Programacion registro;
+    protected ESTADO_CRUD estado = ESTADO_CRUD.NINGUNO;
+    protected LazyScheduleModel scheduleModel;
+
+    @PostConstruct
+    public void init() {
+        scheduleModel = new LazyScheduleModel() {
+            @Override
+            public void loadEvents(LocalDateTime start, LocalDateTime end) {
+                try {
+                    cargarEventos(start,end);
+                }catch(Exception e) {
+                    Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
+                    FacesMessage mensaje = new FacesMessage();
+                    mensaje.setSeverity(FacesMessage.SEVERITY_ERROR);
+                    mensaje.setSummary("Error al cargar los datos" + e.getMessage());
+                    fc.addMessage(null, mensaje);
+                }
+            }
+        };
+    }
+
+    public void cargarEventos(LocalDateTime start, LocalDateTime end) {
+        List<Programacion> eventos = bean.programacionList(idSala, Date.from(start.atZone(ZoneId.systemDefault()).toInstant()),
+                Date.from(end.atZone(ZoneId.systemDefault()).toInstant()));
+
+        for (Programacion e : eventos) {
+            DefaultScheduleEvent event = new DefaultScheduleEvent().builder()
+                    .title(e.getIdPelicula().getNombre())
+                    .startDate(e.getDesde().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime())
+                    .endDate(e.getHasta().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime())
+                    .build();
+            scheduleModel.addEvent(event);
+        }
+    }
+
+    public LazyScheduleModel getScheduleModel() {
+        return scheduleModel;
+    }
+
+    public Long getIdSala() {
+        return idSala;
+    }
+
+    public void setIdSala(Long idSala) {
+        this.idSala = idSala;
+    }
+}
--- src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/control/ProgramacionBean.java
+++ src/main/java/sv/edu/ues/occ/ingenieria/prn335_2024/cine/control/ProgramacionBean.java
@@ -1,11 +1,20 @@
 package sv.edu.ues.occ.ingenieria.prn335_2024.cine.control;
 
+import jakarta.ejb.LocalBean;
+import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import sv.edu.ues.occ.ingenieria.prn335_2024.cine.entity.Programacion;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+@Stateless
+@LocalBean
 public class ProgramacionBean extends AbstractDataPersistence<Programacion> implements Serializable {
     @PersistenceContext(unitName = "CinePU")
     EntityManager em;
@@ -18,4 +27,29 @@ public class ProgramacionBean extends AbstractDataPersistence<Programacion> impl
     public EntityManager getEntityManager() {
         return em;
     }
+
+    public List<Programacion> programacionList(final Long idSala, Date start, Date end ){
+        try{
+            TypedQuery<Programacion> q = em.createNamedQuery("Programacion.ListBySelected", Programacion.class);
+            q.setParameter("idSala", idSala);
+            q.setParameter("start",start);
+            q.setParameter("end",end);
+            return q.getResultList();
+        }catch (Exception e){
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE,e.getMessage(),e);
+        }
+        return List.of();
+    }
+
+    public int contar(final Long idProgramacion){
+        try{
+            TypedQuery<Long> q = em.createNamedQuery("Programacion.cantidadPaginador", Long.class);
+            q.setParameter("idProgramacion", idProgramacion);
+            return q.getSingleResult().intValue();
+        }catch (Exception e){
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE,e.getMessage(),e);
+        }
+        return 0;
+    }
+
 }
